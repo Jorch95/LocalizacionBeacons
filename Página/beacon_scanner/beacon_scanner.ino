e0e213557f3e6f61e069fb8f728519b5cab29b57
matiglesias <matiglesias@users.noreply.github.com>
2017-11-30 02:24:28 -0300

    Agregado todo el escaneo

---
+++ Página/beacon_scanner/beacon_scanner.ino
@@ -0,0 +1,33 @@
+String str;
+String ID;
+String rssi;
+void setup() {
+  Serial.begin(230400); // opens serial port, sets data rate to 9600 bps
+  Serial.setTimeout(1);
+  Serial1.begin(230400);
+  Serial1.setTimeout(3);
+}
+int x = 1;
+
+String serial1Readln(){
+  while(!Serial1.available()){
+    __asm__ __volatile__ ("nop\n\t");
+  }
+  return Serial1.readString();
+}
+
+void loop() {
+  Serial1.println("AT+DISI?");   //envio el comando
+  str = serial1Readln();         // leo mensaje de comienzo de escaneo DISIS pero lo descarto
+  str = serial1Readln();
+  while (!str.endsWith("E")) {
+    str = str.substring(17);    //  ignoro ciertos bytes que no son relevantes
+    if (str.startsWith("BEACBEACBEACBEACBEACBEAC")) {   // filtro solo "nuestros" beacons (24)
+      ID = str.substring(24,32);
+      rssi = str.substring(58);
+      Serial.println(ID);
+      Serial.println(rssi);
+    }
+    str = serial1Readln();
+  }
+}
---
+++ Página/main.py
@@ -0,0 +1,29 @@
+import serial
+from tinydb import TinyDB, where
+from time import time
+import signal, sys
+
+db = TinyDB('db.db')
+arduino = serial.Serial('COM3', baudrate=230400, timeout=1.0)
+
+def handler(signum, frame):
+    db.close()
+    arduino.close()
+    sys.exit()
+signal.signal(signal.SIGINT, handler)
+
+db.purge_tables()
+tabla = db.table('tabla')
+
+while True:
+    while (arduino.inWaiting() == 0):
+        pass
+    ID = arduino.readline().strip()
+
+    segundos = time()
+
+    while (arduino.inWaiting() == 0):
+        pass
+    RSSI = arduino.readline().strip()
+
+    tabla.insert({'ID': ID, 'RSSI': RSSI, 'segundos': segundos})
