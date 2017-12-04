a6422e86bc6b1e793b4654a788d1c1a01cbfe850
Matias Iglesias <matidxg@gmail.com>
2017-12-04 17:42:06 -0300

    Agregados metodos de calobracion backend. No probado porque no anda el frontend

--- Página/webnueva.py
+++ Página/webnueva.py
@@ -16,22 +16,22 @@ IDBeacon = "00000000" # Se usa para que no muestre nada apenas se entra en la pÃ
 intervalo = 100000.0
 IDCalibrado = "00000000" #Se usa para ver la funcionalidad del botÃ³n calibrar.
 
-def obtenerReferenciaRSSSI():
-    #leer desde archivo o calcular con muestras actuales
-    if IDCalibrado == "00000000":
-        return 0
-    return 60
+#busca el "ID" recibido en la tabla de referencias. si lo encuentra lo devuelve, sino devuelve 0 (seria ilogico que un RSSI de ref sea 0).
+def obtenerReferenciaRSSSI(ID):
+    referencia = tablaReferencias.search(where('ID') == ID)
+    if len(referencia) == 1:
+        return referencia['RSSIref']
+    else:
+        return 51
 
-def obtenerPromedioRSSI():
-    global IDBeacon
-    global intervalo
-    registros = tabla.search((where('ID') == IDBeacon) & (where('segundos') > (time() - float(intervalo))))
+#calcula el promedio de RSSIs escaneados correspondientes a "ID" en un lapso de "duracion"
+def obtenerPromedioRSSI(ID, duracion):
+    registros = tabla.search((where('ID') == ID) & (where('segundos') > (time() - float(duracion))))
     suma = 0
     cant = 0
     for registro in registros:
         suma += int(registro['RSSI'])
         cant += 1
-
     if cant > 0:
         return float(suma)/float(cant)
     else:
@@ -42,13 +42,23 @@ def calculoDistancia(rssiPromedio, rssiReferencia):
     dist = sqrt(rssiPromedio/rssiReferencia)
     return dist
 
+#al llamar a este metodo se asume que el celular esta a 1m.
+#se toman muestras del RSSI de "ID" durante 15seg, se calcula su promedio y se lo guarda en la tabla de referencias de RSSIs
+#devuelve el valor de referencia almacenado por si acaso sea de utilidad
+def calibrar(ID):
+    delay(15)
+    rssiPromedio = obtenerPromedioRSSI(ID, 15)
+    tablaReferencias.insert({'ID': ID, 'RSSIref': rssiPromedio})
+    return rssiPromedio
+
 @web.route('/')
 def index():
     global IDBeacon
+    global intervalo
     calibrado = False
     dist = 99999.99
-    rssiPromedio = obtenerPromedioRSSI()
-    rssiReferencia = obtenerReferenciaRSSSI()
+    rssiPromedio = obtenerPromedioRSSI(IDBeacon, intervalo)
+    rssiReferencia = obtenerReferenciaRSSSI(IDBeacon)
     if rssiReferencia > 0:
         calibrado = True
         if rssiPromedio > 0:
